3a2b03b11f07e664811c370f6065f61caca4ddc7
limchaekyung <corud505@naver.com>
2022-01-12 23:57:16 +0900

    graphql mutation

--- graphql/db.js
+++ graphql/db.js
@@ -1,55 +1,9 @@
-export const people = [
-    {
-        id: "0",
-        name: "chae",
-        age: 18,
-        gender: "female"
-    },
-    {
-        id: "1",
-        name: "Japan Guy",
-        age: 18,
-        gender: "male"
-    },
-    {
-        id: "2",
-        name: "JD",
-        age: 18,
-        gender: "male"
-    },
-    {
-        id: "3",
-        name: "moondaddi",
-        age: 18,
-        gender: "female"
-    },
-    {
-        id: "4",
-        name: "flynn",
-        age: 18,
-        gender: "female"
-    },
-    {
-        id: "5",
-        name: "carrie",
-        age: 18,
-        gender: "female"
-    },
-    {
-        id: "6",
-        name: "hyun",
-        age: 18,
-        gender: "male"
-    },
-    {
-        id: "7",
-        name: "lan",
-        age: 18,
-        gender: "female"
-    }
-];
+import fetch from 'node-fetch'
 
-export const getById = id => {
-    const filteredPeople = people.filter(person => id === people.id);
-    return filteredPeople[0];
+const API_URL = "https://yts.mx/api/v2/list_movies.json"
+
+const getMovies = (limit, rating) => {
+    fetch(`${API_URL}`)
+    .then(res => res.json())
+    .then(json => json.data.movies)
 }
