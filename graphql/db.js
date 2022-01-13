ed1a852d592718f95360de49662247ac4cc18868
limchaekyung <corud505@naver.com>
2022-01-14 01:12:57 +0900

    graphql RESTAPI

--- graphql/db.js
+++ graphql/db.js
@@ -1,9 +1,45 @@
-import fetch from 'node-fetch'
+import axios from "axios";
+const BASE_URL = "https://yts.am/api/v2/";
+const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
+const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`;
+const MOVIE_SUGGETIONS_URL = `${BASE_URL}movie_suggestions.json`;
 
-const API_URL = "https://yts.mx/api/v2/list_movies.json"
+export const getMovies = async(limit, rating) => {
+    const{
+        data: {
+            data: {movies}
+        }
+    } = await axios(LIST_MOVIES_URL, {
+        params: {
+            limit,
+            minimum_rating: rating
+        }
+    });
+    return movies;
+};
 
-const getMovies = (limit, rating) => {
-    fetch(`${API_URL}`)
-    .then(res => res.json())
-    .then(json => json.data.movies)
+export const getMovie = async id => {
+    const {
+        data: {
+            data: {movie}
+        }
+    } = await axios(MOVIE_DETAILS_URL, {
+        params: {
+            movie_id: id
+        }
+    });
+    return movie;
+};
+
+export const getSuggestions = async id => {
+    const{
+        data: {
+            data: {movies}
+        }
+    } = await axios(MOVIE_SUGGETIONS_URL, {
+        params: {
+            movie_id: id
+        }
+    });
+    return movies;
 }
